af8369be885e521c3e7675a407437e9edde11053
Jose Ruivo <jruivo.dev@gmail.com>
2017-03-20 16:59:05 +0000

    Added images to each word.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,10 +4,18 @@ package com.example.android.miwok;
 class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceID;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String mDefaultTranslation, String mMiwokTranslation) {
         this.mMiwokTranslation = mMiwokTranslation;
         this.mDefaultTranslation = mDefaultTranslation;
+    }
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceID) {
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mImageResourceID = mImageResourceID;
     }
 
     public String getMiwokTranslation() {
@@ -16,5 +24,13 @@ class Word {
 
     public String getmDefaultTranslation() {
         return mDefaultTranslation;
+    }
+
+    public int getmImageResourceID() {
+        return mImageResourceID;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceID != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -33,6 +34,10 @@ class WordAdapter extends ArrayAdapter<Word> {
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getmDefaultTranslation());
 
+        if (currentWord.hasImage()) {
+            ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+            iconView.setImageResource(currentWord.getmImageResourceID());
+        }
         return listItemView;
     }
 }
